9c6dfd4d95160edf6ddafbc9c6be20bffe0d98d9
Sarah Pai <pai.sarah89@gmail.com>
2019-01-29 16:24:05 -0500

    login successful

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
-	# skip_before_action :authorized, only: [:create]
-  #   before_action :set_user, only: [:destroy]
+	skip_before_action :authorized, only: [:create]
+    before_action :set_user, only: [:destroy]
   
   def profile
     render json: { user: UserSerializer.new(current_user) }, status: :accepted
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-    # before_action :authorized
+    before_action :authorized
    
     def encode_token(payload)
       # should store secret in env variable
@@ -13,10 +13,10 @@ class ApplicationController < ActionController::API
   
     def decoded_token
       if auth_header
-        token = auth_header.split(' ')[1]
+        @token = auth_header.split(' ')[1]
         # header: { 'Authorization': 'Bearer <token>' }
         begin
-          JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
+          JWT.decode(@token, 'my_s3cr3t', true, algorithm: 'HS256')
         rescue JWT::DecodeError
           nil
         end
